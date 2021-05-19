e4a511c1b3a136a6d138928b2eff258b15f62af7
17turquoise <71997148+17turquoise@users.noreply.github.com>
2021-05-20 06:33:09 +0900

    Added footer links

--- src/App.js
+++ src/App.js
@@ -13,7 +13,18 @@ export default function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by SheCodes</small>
+          <small>
+            This project was coded by
+            <a href="https://www.linkedin.com/in/ina-pae-72800978/">Ina Pae</a>
+            and is open-sourced on
+            <a href="https://github.com/17turquoise/dictionary-project">
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a href="https://ecstatic-wiles-1d2cbb.netlify.app/">Netlify </a>
+            Coded by
+            <a href="https://www.shecodes.io/">SheCodes</a>
+          </small>
         </footer>
       </div>
     </div>
