5da76536eb66519dcf3df45b99792022209f10cd
17turquoise <71997148+17turquoise@users.noreply.github.com>
2021-05-21 06:42:57 +0900

    Added Synonyms on the dictionary app

--- src/App.js
+++ src/App.js
@@ -14,16 +14,15 @@ export default function App() {
         </main>
         <footer className="App-footer">
           <small>
-            This project was coded by
-            <a href="https://www.linkedin.com/in/ina-pae-72800978/">Ina Pae</a>
-            and is open-sourced on
+            This project was coded by{" "}
+            <a href="https://www.linkedin.com/in/ina-pae-72800978/">Ina Pae</a>{" "}
+            and is open-sourced on{" "}
             <a href="https://github.com/17turquoise/dictionary-project">
               GitHub
             </a>{" "}
             and hosted on{" "}
-            <a href="https://ecstatic-wiles-1d2cbb.netlify.app/">Netlify </a>
-            Coded by
-            <a href="https://www.shecodes.io/">SheCodes</a>
+            <a href="https://ecstatic-wiles-1d2cbb.netlify.app/">Netlify</a>{" "}
+            Coded by <a href="https://www.shecodes.io/">SheCodes</a>
           </small>
         </footer>
       </div>
